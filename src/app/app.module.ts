5d7a3a037cae97051b740cbcadc2d3e6f71e04a7
DevsNewApp <104905614+DevsNewApp@users.noreply.github.com>
2023-06-26 23:08:56 -0400

    Primera actualizacion
    Esta es la primera actualización

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http"; 
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    HttpClientModule,
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD_g_TzR3eNgpfkvVa11nZa80E_fZDSxIE",
+    authDomain: "tnmtest-501a9.firebaseapp.com",
+    projectId: "tnmtest-501a9",
+    storageBucket: "tnmtest-501a9.appspot.com",
+    messagingSenderId: "578336723106",
+    appId: "1:578336723106:web:ce965929d2020d7d30c7fb",
+    measurementId: "G-BKTZ6MF4PZ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
